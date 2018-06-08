e61d6d2711759dc0f237ec187d2b63318274b511
chamara1995 <kggcbgunarathne@gmail.com>
2018-06-08 10:42:53 +0530

    Add files via upload

---
+++ CO224 - Computer Architecture/lab1/sample.s
@@ -0,0 +1,42 @@
+@ ARM Assembly - sample program 
+
+	.text 	@ instruction memory
+	.global main
+main:
+	@ stack handling, will discuss later
+	@ push (store) lr to the stack
+	sub sp, sp, #4
+	str lr, [sp, #0]
+
+	@ load values --> can be changed
+	mov r0, #10
+	mov r1, #5
+	mov r2, #7
+	mov r3, #-8
+
+	
+	@ Write YOUR CODE HERE
+	@ ---------------------
+	
+	
+
+
+
+	
+	
+	@ ---------------------
+	
+	
+	@ load aguments and print
+	ldr r0, =format
+	mov r1, r5
+	bl printf
+
+	@ stack handling (pop lr from the stack) and return
+	ldr lr, [sp, #0]
+	add sp, sp, #4
+	mov pc, lr
+
+	.data	@ data memory
+format: .asciz "The Answer is %d (Expect XX if correct)\n"
+
