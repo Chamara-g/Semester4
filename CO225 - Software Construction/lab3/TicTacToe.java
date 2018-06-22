ee39e72b68f0e76ed9c4f102196b6c2dcf466bb2
chamara1995 <kggcbgunarathne@gmail.com>
2018-06-22 11:53:16 +0530

    add lab3

---
+++ CO225 - Software Construction/lab3/TicTacToe.java
@@ -0,0 +1,16 @@
+/*
+E/14/108
+Lab3 - TicTacToe
+*/
+
+public class TicTacToe {
+    
+    public static void main(String [] args){
+    
+        View v = new View();
+        Model m = new Model(v);
+        Controller controller = new Controller(v,m);
+        
+    }
+    
+}
